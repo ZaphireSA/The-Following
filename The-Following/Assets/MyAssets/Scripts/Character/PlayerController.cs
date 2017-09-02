0b7bd62813ff36cb29a7c46e445ac7e2c3e26cb2
SONICAY-LAPTOP\Jannie <novamancer@gmail.com>
2017-09-02 18:39:21 +0200

    Implemented model

--- The-Following/Assets/MyAssets/Scripts/Character/PlayerController.cs
+++ The-Following/Assets/MyAssets/Scripts/Character/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     float rollSpeed = 5f;
     Vector3 velocity = Vector3.zero;
+    Animator anim;
     // Use this for initialization
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -21,9 +23,17 @@ public class PlayerController : MonoBehaviour {
         var vertical = Input.GetAxis("Vertical");
         var horizontal = Input.GetAxis("Horizontal");
         moveDirection = new Vector3(horizontal, 0, vertical);
-        controller.SimpleMove(moveDirection * rollSpeed * Time.deltaTime);
+        controller.SimpleMove(moveDirection * rollSpeed * Time.deltaTime);        
+        
         if (moveDirection != Vector3.zero)
+        {
             transform.rotation = Quaternion.LookRotation(moveDirection);
+            anim.SetFloat("SpeedZ", 1f);
+        } else
+        {
+            anim.SetFloat("SpeedZ", 0f);
+        }
+            
         velocity = Vector3.zero;
     }
 }
