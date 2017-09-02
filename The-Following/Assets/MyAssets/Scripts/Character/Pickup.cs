a5970d7791194148c4097dfdc0681fa6cfcd1c6e
SONICAY-LAPTOP\Jannie <novamancer@gmail.com>
2017-09-02 20:06:59 +0200

    Added player pickup and evolution

---
+++ The-Following/Assets/MyAssets/Scripts/Character/Pickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    [SerializeField]
+    int amount;
+
+    enum Type
+    {
+        Protein
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Player>();
+        if (player != null)
+        {
+            player.GiveProtein(amount);
+            Destroy(transform.parent.gameObject);
+        }
+    }
+
+}
