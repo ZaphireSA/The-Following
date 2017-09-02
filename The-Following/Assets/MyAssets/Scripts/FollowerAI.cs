22206aa2c49455e126db788f30d5e1c5b3438deb
SONICAY-LAPTOP\Jannie <novamancer@gmail.com>
2017-09-02 17:26:02 +0200

    Added PlayerController Added following functionality

---
+++ The-Following/Assets/MyAssets/Scripts/FollowerAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowerAI : MonoBehaviour {
+
+    [SerializeField]
+    NavMeshAgent agent;
+
+    [SerializeField]
+    float walkSpeed = 5f;
+    [SerializeField]
+    float runSpeed = 10f;
+    Vector3 targetPos;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        
+        targetPos = transform.position;
+        agent.speed = walkSpeed;
+        SetTarget(transform.position);
+        //StartCoroutine(Roam());
+    }
+
+    public void SetTarget(Vector3 pos)
+    {
+        //Debug.DrawLine(fleePoint, fleePoint + new Vector3(0, 5, 0),Color.blue, 3f);
+        NavMeshHit hit;
+        NavMesh.SamplePosition(pos, out hit, 3, 1);
+        targetPos = hit.position;
+
+        //targetPos = RandomNavSphere(transform.position, maxRoamDistance, 1);
+        agent.SetDestination(targetPos);
+    }
+
+}
